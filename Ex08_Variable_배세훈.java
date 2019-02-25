957d5ea005832e522846c7731bb9997d7b08227d
SeHoonBae <bey1548@naver.com>
2019-02-25 10:03:37 +0900

    변수

---
+++ Ex08_Variable_배세훈.java
@@ -0,0 +1,211 @@
+class Ex08_Variable_배세훈 {
+	public static void main(String[] args) {
+		
+		int phoneCapacity=32;
+		int mouseDpi=1600;
+		int bookPrice=30000;
+		int brushNumber=1;
+		int cupSize=100;
+		int computerRam=16;
+		int wifiTransmissionRate=5;
+		int mathScore=90;
+		int scienceScore=80;
+		int korScore=70;
+	
+		System.out.println("제 휴대폰 용량은"+phoneCapacity+"입니다.");
+		System.out.println("제 마우스 DPI는"+mouseDpi+"입니다.");
+		System.out.println("책 가격은"+bookPrice+"입니다.");
+		System.out.println("제 칫솔 개수는"+brushNumber+"입니다.");
+		System.out.println("제 컵 크기는"+cupSize+"입니다.");
+		System.out.println("제 컴퓨터 램은"+computerRam+"GB입니다.");
+		System.out.println("와이파이 전송속도는"+wifiTransmissionRate+"G입니다.");
+		System.out.println("수학점수는"+mathScore+"입니다.");
+		System.out.println("과학점수는"+scienceScore+"입니다.");
+		System.out.println("국어점수는"+korScore+"입니다.");
+
+		double mySight=0.8;
+		double sharpLead=1.2;
+		double weight=79.5;
+		double height=176.5;
+		double fingerLength=6.5;
+		double toeLength=4.2;
+		double bridgeLength = 500.12;
+		double blackboardColumnSize = 50.8;
+		double blackboardWidthSize = 120.5;
+		double nailLength=2.3;
+
+
+		System.out.println("제 시력은"+mySight+"입니다.");
+		System.out.println("샤프심 길이는"+sharpLead+"입니다.");
+		System.out.println("몸무게는"+weight+"입니다.");
+		System.out.println("키는"+height+"입니다.");
+		System.out.println("손가락 길이는"+fingerLength+"입니다.");
+		System.out.println("발가락 길이는"+toeLength+"입니다.");
+		System.out.println("다리 길이는"+bridgeLength+"입니다.");
+		System.out.println("칠판 세로크기는"+blackboardColumnSize+"입니다.");
+		System.out.println("칠판 가로크기는"+blackboardWidthSize+"입니다.");
+		System.out.println("손톱 길이는"+nailLength+"입니다.");
+
+
+		byte heatingTemperature = 24;
+		byte airConditioner = 20;
+		byte customerNumber = 100;
+		byte remotecontrolSize = 20;
+		byte remotecontrolButtonNumber = 25;
+		byte tvChannelNumber=120;
+		byte tvCableNumber=3;
+		byte studentNumber=14;
+		byte studyTime=8;
+		byte subjectNumber=1;
+
+		System.out.println("난방 온도는"+heatingTemperature+"입니다.");
+		System.out.println("에어컨 온도는"+airConditioner+"입니다.");
+		System.out.println("고객수는"+customerNumber+"입니다.");
+		System.out.println("리모컨 크기는"+remotecontrolSize+"입니다.");
+		System.out.println("리모컨 버튼수는"+remotecontrolButtonNumber+"입니다.");
+		System.out.println("티비 채널수는"+tvChannelNumber+"입니다.");
+		System.out.println("티비 연결선 수는"+tvCableNumber+"입니다.");
+		System.out.println("학생수는"+studentNumber+"입니다.");
+		System.out.println("공부시간은"+studyTime+"입니다.");
+		System.out.println("과목수는"+subjectNumber+"입니다.");
+
+		short glassPrice=30000;
+		short clothesPrice = 15000;
+		short bussPrice = 8000;
+		short bussSeatNumber = 8;
+		short chairPrice = 25000;
+		short chairLegNumber =4;
+		short ClassTableNumber = 10;
+		short ClassChairNumber = 30;
+		short ClassNumber = 5;
+		short TeacherNumber =1;
+
+		System.out.println("안경 가격은 "+glassPrice+"입니다.");
+		System.out.println("옷 가격은 "+glassPrice+"입니다.");
+		System.out.println("버스 가격은 "+bussPrice+"입니다.");
+		System.out.println("좌석번호는 "+bussSeatNumber+"입니다.");
+		System.out.println("의자 가격은 "+chairPrice+"입니다.");
+		System.out.println("의자 다리 수는 "+chairLegNumber+"입니다.");
+		System.out.println("반에 있는 책상 수는 "+ClassTableNumber+"입니다.");
+		System.out.println("반에있는 의자 수는 "+ClassChairNumber+"입니다.");
+		System.out.println("반은 "+ClassNumber+"개 있습니다.");
+		System.out.println("선생님은 "+TeacherNumber+"명 입니다.");
+
+		char ya='야';
+		char ho='호';
+		char bae='배';
+		char se='세';
+		char hoon='훈';
+		char ja='자';
+		char va='바';
+		char book='책';
+		char cup ='컵';
+		char pen='펜';
+
+		System.out.println("ya 한글발음 : "+ya+"입니다.");
+		System.out.println("ho 한글발음 : "+ho+"입니다.");
+		System.out.println("bae 한글발음 : "+bae+"입니다.");
+		System.out.println("se 한글발음 : "+se+"입니다.");
+		System.out.println("hoon 한글발음 : "+hoon+"입니다.");
+		System.out.println("ja 한글발음 : "+ja+"입니다.");
+		System.out.println("va 한글발음 : "+va+"입니다.");
+		System.out.println("book은 한글로 "+book+"입니다.");
+		System.out.println("cup은 한글로 "+cup+"입니다.");
+		System.out.println("pen는 한글로 "+pen+"입니다.");
+
+		long busanDistance=300L;
+		long chinaDistance=1000L;
+		long gumiDistance= 330L;
+		long inchonDistance=100L;
+		long daeguDistance=200L;
+		long daejeonDistance=280L;
+		long andongDistance=290L;
+		long unDistance=2000L;
+		long australiaDistance=1500L;
+		long japanDistance=1100L;
+
+		System.out.println("부산까지 거리는"+busanDistance+"km입니다.");
+		System.out.println("중국까지 거리는"+chinaDistance+"km입니다.");
+		System.out.println("구미까지 거리는"+gumiDistance+"km입니다.");
+		System.out.println("인천까지 거리는"+inchonDistance+"km입니다.");
+		System.out.println("대전까지 거리는 거리는"+daejeonDistance+"km입니다.");
+		System.out.println("대구까지 거리는"+daeguDistance+"km입니다.");
+		System.out.println("안동까지 거리는"+andongDistance+"km입니다.");
+		System.out.println("미국까지 거리는"+unDistance+"km입니다.");
+		System.out.println("호주까지 거리는"+australiaDistance+"km입니다.");
+		System.out.println("일본까지 거리는"+japanDistance+"km입니다.");
+
+		float gangnam =10.1f;
+		float sinchon =20.3f;
+		float seoulNationalUniversity =12.3f;
+		float nakseongdae = 19.9f;
+		float sindorim= 25.9f;
+		float daelim=30.9f;
+		float sinrim=40.8f;
+		float bongcheon=7.7f;
+		float jamsil=50.8f;
+		float yeongdeungpo=120.9f;
+
+
+		System.out.println("강남까지 거리는"+gangnam+"km입니다.");
+		System.out.println("까지 거리는"+sinchon+"km입니다.");
+		System.out.println("까지 거리는"+seoulNationalUniversity+"km입니다.");
+		System.out.println("까지 거리는"+nakseongdae+"km입니다.");
+		System.out.println("까지 거리는"+daelim+"km입니다.");
+		System.out.println("까지 거리는"+sinrim+"km입니다.");
+		System.out.println("까지 거리는"+sindorim+"km입니다.");
+		System.out.println("까지 거리는"+bongcheon+"km입니다.");
+		System.out.println("까지 거리는"+jamsil+"km입니다.");
+		System.out.println("까지 거리는"+yeongdeungpo+"km입니다.");
+
+		boolean isMale=true;
+		boolean isFemale=false;
+		boolean isHuman=true;
+		boolean isDog=false;
+		boolean isLion=false;
+		boolean isAnimal=true;
+		boolean isPlant=false;
+		boolean isBook=true;
+		boolean isMouse=true;
+		boolean isComputer=true;
+
+		System.out.println("남자입니까?: "+isMale);
+		System.out.println("여자입니까?: "+isFemale);
+		System.out.println("사람입니까?:"+isHuman);
+		System.out.println("당신은 개를 키우고있습니까?"+isDog);
+		System.out.println("당신은 사자를 좋아합니까?:"+isLion);
+		System.out.println("당신은 동물을 좋아합니까?:"+isAnimal);
+		System.out.println("식물을 키우고 있습니까?:"+isPlant);
+		System.out.println("책을 가지고 있습니까?:"+isBook);
+		System.out.println("전용 마우스가 있습니까?:"+isMouse);
+		System.out.println("컴퓨터를 가지고 있습니까?:"+isComputer);
+
+		String name="sehoon";
+		String myHighSchool="현일";
+		String myMiddleSchool="선주";
+		String academyName="쌍용교육센터";
+		String subjectName="자바";
+		String bookName="이것이 자바다";
+		String computerBrand="msi";
+		String homeTown="구미";
+		String hobby="요리";
+		String favoriteKindCook="삼겹살";
+
+		System.out.println("내 이름은"+name+"입니다.");
+		System.out.println("출신 고등학교 이름은?"+myHighSchool+"입니다.");
+		System.out.println("출신 중학교 이름은?"+myMiddleSchool+"입니다.");
+		System.out.println("다니고 있는 학원 이름은?"+academyName+"입니다.");
+		System.out.println("배우고 있는 과목명은?"+subjectName+"입니다.");
+		System.out.println("배우고 있는 책 이름은?"+bookName+"입니다.");
+		System.out.println("노트북 브랜드 이름은?"+computerBrand+"입니다.");
+		System.out.println("내 고향은"+homeTown+"입니다.");
+		System.out.println("내 취미는"+hobby+"입니다.");
+		System.out.println("가장 좋아하는 음식은"+favoriteKindCook+"입니다.");
+	}
+}
+
+// 1. 주변의 데이터 찾기
+// 2. 적당한 자료형 선정
+// 3. 변수 생성+값 할당(상수)
+// 4. 출력(문장 생성)
+// 9가지 자료형 10개씩
